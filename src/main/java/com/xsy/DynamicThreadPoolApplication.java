49719484f47f84ae461b926956c73886e3470d25
桑榆 <11166325+xingshengyang@user.noreply.gitee.com>
2025-11-11 16:48:37 +0800

    动态线程池的实现

---
+++ src/main/java/com/xsy/DynamicThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.xsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicThreadPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicThreadPoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsy/DynamicThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicThreadPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
